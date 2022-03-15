12510b8fb318ee219166b11ba9e42c19980339b1
Ricardo Romanach <rjromanach@gmail.com>
2022-03-15 02:30:03 -0500

    Added many configurations and cave compatibility
    New configs: only show for host, stat formats, stat abbreviations, show health penalties, show player number, hide your own stats
    We also added some console commands to make inter-shard communication easier, such as RefillStats, ShardRevivePlayer, and ShardGodMode. This utilizes a very dangerous ShardInjection RPC.

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
-name = "Player Status (server)"
-description = "View other player's status, including some other properties. Press \"\\\" to toggle the list.\nIf you want to manually change the max number of players shown, you can use the command ChangeScale(maxplayersnumber)."
+name = "Player Status"
+description = "View other player's status, including some other properties. By default, press \"\\\" to toggle the list.\nIf you want to manually change the font size, you can use the command ChangeScale(*number*)."
 author = "rawii22 & lord_of_les_ralph"
-version = "2.0.0.21"
+version = "3.0.0.0"
 icon = "modicon.tex"
 icon_atlas = "modicon.xml"
 
@@ -18,18 +18,112 @@ configuration_options =
 {
 	{
 		name = "HOSTONLY",
-		label = "Show only for host?",
-		hover = "Let's you chose if you want others to see the player stats list.",
+		label = "Show only for host",
+		hover = "Lets you chose if you want others to see the player stats list.",
+		default = true,
 		options = {
 			{ description = "No", data = false},
 			{ description = "Yes", data = true},
 		},
-		default = true
+	},
+	{
+		name = "TOGGLEKEY",
+		label = "Toggle Player Stats",
+		default = "\\",
+		options = {
+			{description = "A", data = "A"},
+			{description = "B", data = "B"},
+			{description = "C", data = "C"},
+			{description = "D", data = "D"},
+			{description = "E", data = "E"},
+			{description = "F", data = "F"},
+			{description = "G", data = "G"},
+			{description = "H", data = "H"},
+			{description = "I", data = "I"},
+			{description = "J", data = "J"},
+			{description = "K", data = "K"},
+			{description = "L", data = "L"},
+			{description = "M", data = "M"},
+			{description = "N", data = "N"},
+			{description = "O", data = "O"},
+			{description = "P", data = "P"},
+			{description = "Q", data = "Q"},
+			{description = "R", data = "R"},
+			{description = "S", data = "S"},
+			{description = "T", data = "T"},
+			{description = "U", data = "U"},
+			{description = "V", data = "V"},
+			{description = "W", data = "W"},
+			{description = "X", data = "X"},
+			{description = "Y", data = "Y"},
+			{description = "Z", data = "Z"},
+			{description = "[", data = "["},
+			{description = "]", data = "]"},
+			{description = "\\", data ="\\"},
+			{description = ";", data = ";"},
+			{description = "\'", data ="\'"},
+			{description = "/", data = "/"},
+			{description = "-", data = "-"},
+			{description = "=", data = "="}
+		}
+	},
+	{
+		name = "ABBREVIATESTATS",
+		label = "Stat names",
+		hover = "Abbreviated: HG:# | S:# | HP:#\nNormal: Hunger:# | Sanity:# | Health:#",
+		default = {HUNGER = "HG:", SANITY = "S:", HEALTH = "HP:"},
+		options = {
+			{ description = "Abbreviated", data = {HUNGER = "HG:", SANITY = "S:", HEALTH = "HP:"}},
+			{ description = "Normal", data = {HUNGER = "Hunger:", SANITY = "Sanity:", HEALTH = "Health:"}},
+			{ description = "None", data = {HUNGER = "", SANITY = "", HEALTH = ""}},
+		},
+	},
+	{
+		name = "STATNUMFORMAT",
+		label = "Stat number format",
+		hover = "Fraction: HG: current/max\tPercent: HG: 100%\nBoth: HG: current/max (100%)",
+		default = "$current/$maximum",
+		options = {
+			{ description = "Both", data = "$current/$maximum ($percent%)"},
+			{ description = "Fraction", data = "$current/$maximum"},
+			{ description = "Percent", data = "$percent%"},
+		},
+	},
+	{
+		name = "SHOWPENALTY",
+		label = "Show stat penalties",
+		hover = "If enabled, you will see health and sanity penalty percentages next to their respective stats.",
+		default = true,
+		options = {
+			{ description = "Yes", data = true},
+			{ description = "No", data = false},
+		},
+	},
+	{
+		name = "SHOWPLAYERNUMS",
+		label = "Show player numbers",
+		hover = "If enabled, you will see the number of each player.",
+		default = true,
+		options = {
+			{ description = "Yes", data = true},
+			{ description = "No", data = false},
+		},
+	},
+	{
+		name = "HIDEOWNSTATS",
+		label = "Hide your own stats",
+		hover = "If you select yes, your own stats will be hidden from the player stats list.",
+		default = false,
+		options = {
+			{ description = "Yes", data = true},
+			{ description = "No", data = false},
+		},
 	},
 	{
 		name = "SCALE",
-		label = "Max num players visible",
-		hover = "Scales text size. You can change if your server only allows a small number of people.",
+		label = "Text size",
+		hover = "Scales text size. The bigger the number, the smaller the text. Size can also be changed manually with the console command ChangeScale(*number*)",
+		default = 36.5,
 		options = {
 			{ description = "6 (big)", data = 115},
 			{ description = "12", data = 60},
@@ -37,6 +131,5 @@ configuration_options =
 			{ description = "30 (smaaaall)", data = 24.3},
 			{ description = "50 (micro)", data = 14.6},
 		},
-		default = 36.5
 	},
 }
--- modmain.lua
+++ modmain.lua
@@ -1,19 +1,33 @@
 local Widget = GLOBAL.require("widgets/widget")
 local Text = GLOBAL.require("widgets/text")
+local json = GLOBAL.json
+local pcall = GLOBAL.pcall
 local HOSTONLY = GetModConfigData("HOSTONLY")
+local TOGGLEKEY = GetModConfigData("TOGGLEKEY")
+local SHOWPENALTY = GetModConfigData("SHOWPENALTY")
+local SHOWPLAYERNUMS = GetModConfigData("SHOWPLAYERNUMS")
+local HIDEOWNSTATS = GetModConfigData("HIDEOWNSTATS")
+local STATNUMFORMAT = GetModConfigData("STATNUMFORMAT")
+local STATSTEXT = GetModConfigData("ABBREVIATESTATS")
 local SCALE = GetModConfigData("SCALE")
 local TheWorld --since the world won't exist until later, we must define the shortcut later
+local externalPlayerList
+local closeMessage = "Press \""..TOGGLEKEY.."\" to close\n"
 
 local playerData
 
+function IsEnabled()
+	return (HOSTONLY and GLOBAL.TheNet:GetIsServerAdmin()) or not HOSTONLY --for some reason, we cannot use ismastersim, so we must use a TheNet function
+end
+
 AddPrefabPostInit("player_classified", function(player)
 	TheWorld = GLOBAL.TheWorld
+	--This netvar is for sending the player data from the server to clients (including the host when necessary)
 	player._playerDataString = GLOBAL.net_string(player.GUID, "_playerDataString", "playerdatadirty")
 	
-	if TheWorld.ismastersim then
-		player:DoPeriodicTask(30 * GLOBAL.FRAMES, function() RefreshText(player) end)
-	elseif IsEnabled() then  --taking advantage of the ismastersim check in one place
+	if IsEnabled() then  --taking advantage of the ismastersim check in one place
 		player:ListenForEvent("playerdatadirty", RefreshClientText, player)
+		print("player._parent: "..tostring(player._parent))
 	end
 end)
 
@@ -24,13 +38,27 @@ AddSimPostInit(function()
 		
 		local startPos = CalcBasePos()
 		playerData:SetPosition(startPos)--(3600,900,0)
-		playerData:SetHAlign(GLOBAL.ANCHOR_RIGHT)
-		playerData:SetAlpha(.7)
+		playerData:SetHAlign(GLOBAL.ANCHOR_LEFT)
+		playerData:SetAlpha(.8)
+
+		if GLOBAL.TheWorld.ismastersim then
+			if GLOBAL.TheShard:IsMaster() or GLOBAL.TheShard:IsSecondary() then
+				GLOBAL.TheWorld:DoPeriodicTask(30 * GLOBAL.FRAMES, function()
+					local r, result = pcall(json.encode, GetPlayerData(GLOBAL.AllPlayers, true))
+					if not r then print("[Player Status] Could not encode player stat data.") end
+					if result then
+						SendModRPCToShard(GetShardModRPC(modname, "SendPlayerList"), nil, nil, nil, result)
+					end
+				end)
+			else
+				GLOBAL.TheWorld:DoPeriodicTask(30 * GLOBAL.FRAMES, function() RefreshText(GetPlayerData(GLOBAL.AllPlayers)) end)
+			end
+		end
 	end
 end)
 
 function CreateText()
-	playerData = Text("stint-ucr", SCALE, GetPlayerData())
+	playerData = Text("stint-ucr", SCALE, GetPlayerData(GLOBAL.AllPlayers))
 end
 
 --for testing. calculated using the points from the config options using Desmos
@@ -39,13 +67,18 @@ function GLOBAL.ChangeScale(playercount)
 	playerData:SetSize(size)
 end
 
-function RefreshText(player)
-	local data = GetPlayerData()
+--update the text and then update netvar for clients. This will trigger the netvar's dirty function, RefreshClientText
+function RefreshText(data)
 	playerData:SetString(data)
 	playerData:SetPosition(CalcBasePos())
-	player._playerDataString:set(data)
+	for k,player in pairs(GLOBAL.AllPlayers) do
+		if player.player_classified then
+			player.player_classified._playerDataString:set(data)
+		end
+	end
 end
 
+--updates the text widget for clients
 function RefreshClientText(player)
 	local data = player._playerDataString:value()
 	playerData:SetString(data)
@@ -53,21 +86,54 @@ function RefreshClientText(player)
 end
 
 --only run on the host and then sent to the client via netvar in the prefab post init
-function GetPlayerData()
-	local data = "Press \"\\\" to close\n"
-	--[[
-	for k=1, 100 do
-		data = data..k.."\n"
-	end]]
-	for k,player in pairs(GLOBAL.AllPlayers) do
-		data = data..k..": "..player.name
-		.." | creative: "..tostring(player.components.builder.freebuildmode)
-		.." | godmode: "..tostring(player.components.health.invincible).."\n"
-		.."hunger: "..math.floor(player.components.hunger.current+0.5)
-		.."  sanity: "..math.floor(player.components.sanity.current+0.5)
-		.."  health: "..(player:HasTag("playerghost") and "0 (dead)" or math.floor(player.components.health.currenthealth+0.5)).."\n"
+function GetPlayerData(players, asTable)
+	
+	local statString = (asTable and "" or closeMessage)
+	local statTable = {}
+	
+	for k,player in pairs(players) do
+		local currentStat = (SHOWPLAYERNUMS and not asTable and k..": " or "")
+		if HIDEOWNSTATS and player.GUID == GLOBAL.ThePlayer.GUID then
+			currentStat = currentStat..(SHOWPLAYERNUMS and "[You]" or "")
+		else
+			local hungerStats = STATSTEXT.HUNGER.." "..string.gsub(STATNUMFORMAT, "%$(%w+)", 
+				{
+					current=(player:HasTag("playerghost") and "0" or math.floor(player.components.hunger.current+0.5)),
+					maximum=player.components.hunger.max,
+					percent=math.floor((player:HasTag("playerghost") and 0 or player.components.hunger.current)/player.components.hunger.max*100+0.5),
+				})
+			local sanityStats = " | "..STATSTEXT.SANITY.." "..string.gsub(STATNUMFORMAT, "%$(%w+)", 
+				{
+					current=(player:HasTag("playerghost") and "0" or math.floor(player.components.sanity.current+0.5)),
+					maximum=player.components.sanity.max,
+					percent=math.floor((player:HasTag("playerghost") and 0 or player.components.sanity.current)/player.components.sanity.max*100+0.5),
+					
+				})
+			local healthStats = player.components.health and " | "..STATSTEXT.HEALTH.." "..string.gsub(STATNUMFORMAT, "%$(%w+)", 
+				{
+					current=(player:HasTag("playerghost") and "0" or math.floor(player.components.health.currenthealth+0.5)),
+					maximum=math.floor(player.components.health.maxhealth*(1-player.components.health.penalty)+0.5),
+					percent=math.floor((player:HasTag("playerghost") and 0 or player.components.health.currenthealth)/player.components.health.maxhealth*100+0.5),
+				}) or ""
+				
+			--add age stat here later for Wanda users
+			currentStat = currentStat..player.name.." ("..player.prefab..")"..(player:HasTag("playerghost") and " [DEAD]" or "")
+			..((player.components.builder.freebuildmode or player.components.health.invincible) and " |" or "")
+			..(player.components.builder.freebuildmode and " [Free-crafting]" or "")
+			..(player.components.health.invincible and " [God-mode]" or "").."\n"
+			..hungerStats
+			..sanityStats..((SHOWPENALTY and player.components.sanity.penalty > 0) and " (-"..math.floor(player.components.sanity.penalty*100+0.5).."%)" or "")
+			..healthStats..((SHOWPENALTY and player.components.health.penalty > 0) and " (-"..math.floor(player.components.health.penalty*100+0.5).."%)" or "").."\n"
+		end
+		--this is awful, we were lazy
+		if asTable then
+			table.insert(statTable, currentStat)
+		else
+			statString = statString..currentStat
+		end
 	end
-	return data
+		
+	return (asTable and statTable or statString)
 end
 
 --must use proportions instead of coordinates since we won't know the screen size.
@@ -105,10 +171,6 @@ AddClassPostConstruct("widgets/controls", function(controls)
 	end
 end)
 
-function IsEnabled()
-	return (HOSTONLY and GLOBAL.TheNet:GetIsServerAdmin()) or not HOSTONLY --for some reason, we cannot use ismastersim, so we must use a TheNet function
-end
-
 --just something that came with the AddKeyUpHandler from some other mod (don't remember which...)
 function IsDefaultScreen()
 	if GLOBAL.TheFrontEnd:GetActiveScreen() and GLOBAL.TheFrontEnd:GetActiveScreen().name and type(GLOBAL.TheFrontEnd:GetActiveScreen().name) == "string" and GLOBAL.TheFrontEnd:GetActiveScreen().name == "HUD" then
@@ -118,9 +180,8 @@ function IsDefaultScreen()
 	end
 end
 
-local toggle = "\\"
 GLOBAL.TheInput:AddKeyUpHandler(
-	toggle:lower():byte(), 
+	TOGGLEKEY:lower():byte(), 
 	function()
 		if not GLOBAL.IsPaused() and IsDefaultScreen() and IsEnabled() then
 			if playerData:IsVisible() then
@@ -131,3 +192,79 @@ GLOBAL.TheInput:AddKeyUpHandler(
 		end
 	end
 )
+
+local playerList
+
+--shardId is ID of the shard that sent the RPC
+--This is what receives the list of players from the opposite shard and combines them in the proper order (overworld first, then cave players) then saves it to the netvar through RefreshText.
+AddShardModRPCHandler(modname, "SendPlayerList", function(shardId, namespace, code, externalPlayerListJson)
+	local r
+	r, externalPlayerList = pcall(json.decode, externalPlayerListJson)
+	if not r then print("Could not decode all items: "..tostring(externalPlayerListJson)) end
+	if externalPlayerList then
+		-- Only run if the shard calling the RPC is different from current shard
+		if GLOBAL.TheShard:GetShardId() ~= tostring(shardId) then
+			local playerStatString = ""
+			if GLOBAL.TheShard:IsMaster() then
+				playerStatString = GetPlayerData(GLOBAL.AllPlayers)
+				for k, player in pairs(externalPlayerList) do
+					playerStatString = playerStatString..(SHOWPLAYERNUMS and ((#GLOBAL.AllPlayers + k)..": ") or "").."**"..player --the "**" indicates players in caves
+				end
+			else
+				playerStatString = closeMessage
+				for k, player in pairs(externalPlayerList) do
+					playerStatString = playerStatString..(SHOWPLAYERNUMS and (k..": ") or "")..player
+				end
+				for k, player in pairs(GetPlayerData(GLOBAL.AllPlayers, true)) do
+					playerStatString = playerStatString..(SHOWPLAYERNUMS and ((#externalPlayerList + k)..": ") or "").."**"..player
+				end
+			end
+			RefreshText(playerStatString)
+		end
+	end
+end)
+
+-----------ADDITIONAL CONSOLE FUNCTIONS-----------
+
+function GLOBAL.ShardRevivePlayer(playerNum)
+	SendModRPCToShard(GetShardModRPC(modname, "ShardInjection"), nil, nil, nil, "AllPlayers["..playerNum.."]:PushEvent(\"respawnfromghost\")")
+end
+
+function GLOBAL.ShardRefillStats(playerNum)
+	SendModRPCToShard(GetShardModRPC(modname, "ShardInjection"), nil, nil, nil, "RefillStats("..playerNum..")")
+end
+
+--This function expects to receive a player number based on what the player stat list shows (aka the server's version of AllPlayers)
+function GLOBAL.RefillStats(playerNum)
+	local shardPlayerNum = playerNum
+	if GLOBAL.TheShard:IsSecondary() then
+		shardPlayerNum = playerNum - #externalPlayerList --if we're in the caves, subtract the number of people in the overworld
+	end
+	if GLOBAL.TheShard:IsMaster() and playerNum > #GLOBAL.AllPlayers then --if called from the overworld, send the function to the caves
+		GLOBAL.ShardRefillStats(playerNum)
+		return
+	elseif GLOBAL.TheShard:IsSecondary() and shardPlayerNum <= 0 then --if called from the caves, send the function to the caves
+		GLOBAL.ShardRefillStats(playerNum)
+		return
+	end
+	if GLOBAL.TheWorld.ismastersim and GLOBAL.AllPlayers[shardPlayerNum] and not GLOBAL.AllPlayers[shardPlayerNum]:HasTag("playerghost") then
+		GLOBAL.AllPlayers[shardPlayerNum].components.health:SetPenalty(0)
+		GLOBAL.AllPlayers[shardPlayerNum].components.health:SetPercent(1)
+		GLOBAL.AllPlayers[shardPlayerNum].components.sanity:SetPercent(1)
+		GLOBAL.AllPlayers[shardPlayerNum].components.hunger:SetPercent(1)
+		GLOBAL.AllPlayers[shardPlayerNum].components.temperature:SetTemperature(25)
+		GLOBAL.AllPlayers[shardPlayerNum].components.moisture:SetPercent(0)
+	end
+end
+
+function GLOBAL.ShardGodmode(playerNum)
+	SendModRPCToShard(GetShardModRPC(modname, "ShardInjection"), nil, nil, nil, "AllPlayers["..playerNum.."].components.health.invincible = true")
+end
+
+--very dangerous RPC, which is why it's not accessible from the console
+AddShardModRPCHandler(modname, "ShardInjection", function(shardId, namespace, code, injection)
+	-- Only run if the shard calling the RPC is different from current shard
+	if GLOBAL.TheShard:GetShardId() ~= tostring(shardId) then
+		GLOBAL.ExecuteConsoleCommand(injection) --just for fun...
+	end
+end)
